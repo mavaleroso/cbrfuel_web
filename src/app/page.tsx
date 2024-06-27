63ef8f549c2250fd8b91d37eb3c72d68085f3209
Marwen Valeroso <marwen-valeroso@crescode.com>
2024-06-27 19:22:51 +0800

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,37 +24,17 @@ import {
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
 
-const products = [
-  { name: "Analytics", description: "Get a better understanding of your traffic", href: "#", icon: ChartPieIcon },
-  { name: "Engagement", description: "Speak directly to your customers", href: "#", icon: CursorArrowRaysIcon },
-  { name: "Security", description: "Your customers’ data will be safe and secure", href: "#", icon: FingerPrintIcon },
-  { name: "Integrations", description: "Connect with third-party tools", href: "#", icon: SquaresPlusIcon },
-  { name: "Automations", description: "Build strategic funnels that will convert", href: "#", icon: ArrowPathIcon },
-];
-const callsToAction = [
-  { name: "Watch demo", href: "#", icon: PlayCircleIcon },
-  { name: "Contact sales", href: "#", icon: PhoneIcon },
-];
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <div>
-      <header className="bg-white">
+      <header className="bg-white relative h-screen">
         <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
+              <Image className="h-14 w-auto" src="/images/cb_ar_fuel_logo_main.png" alt="" width={100} height={100} />
             </a>
           </div>
           <div className="flex lg:hidden">
@@ -148,15 +128,95 @@ export default function Example() {
             </div>
           </DialogPanel>
         </Dialog>
-        <Image
-          src={"/images/fuel_station"}
-          alt="Picture of the author"
-          width={500}
-          height={500}
-          // blurDataURL="data:..." automatically provided
-          // placeholder="blur" // Optional blur-up while loading
-        />
+        <div className="">
+          <Image src="/images/fuel_station.png" alt="Picture of the author" fill={true} />
+        </div>
+        <div className="absolute top-40 left-0 lg:px-36 bg-opacity-5 tracking-widest text-primary ">
+          <p className="text-6xl font-extrabold mb-3 drop-shadow-md stroke-white">Locate</p>
+          <p className="text-6xl font-extrabold mb-3 drop-shadow-md stroke-white">and Fill your</p>
+          <p className="text-6xl font-extrabold mb-3 drop-shadow-md stroke-white">Tanks at Nearby</p>
+          <p className="text-6xl font-extrabold mb-3 drop-shadow-md stroke-white">Stations with Ease</p>
+        </div>
       </header>
+      <section className="relative">
+        <div className="py-24 sm:py-32 ">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-4">
+              <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+                <div className="flex gap-x-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-11 text-primary"
+                  >
+                    <path d="M11.584 2.376a.75.75 0 0 1 .832 0l9 6a.75.75 0 1 1-.832 1.248L12 3.901 3.416 9.624a.75.75 0 0 1-.832-1.248l9-6Z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M20.25 10.332v9.918H21a.75.75 0 0 1 0 1.5H3a.75.75 0 0 1 0-1.5h.75v-9.918a.75.75 0 0 1 .634-.74A49.109 49.109 0 0 1 12 9c2.59 0 5.134.202 7.616.592a.75.75 0 0 1 .634.74Zm-7.5 2.418a.75.75 0 0 0-1.5 0v6.75a.75.75 0 0 0 1.5 0v-6.75Zm3-.75a.75.75 0 0 1 .75.75v6.75a.75.75 0 0 1-1.5 0v-6.75a.75.75 0 0 1 .75-.75ZM9 12.75a.75.75 0 0 0-1.5 0v6.75a.75.75 0 0 0 1.5 0v-6.75Z"
+                      clipRule="evenodd"
+                    />
+                    <path d="M12 7.875a1.125 1.125 0 1 0 0-2.25 1.125 1.125 0 0 0 0 2.25Z" />
+                  </svg>
+                  <dd className="text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">20+</dd>
+                </div>
+                <dt className="text-base leading-7 text-gray-600">Branches</dt>
+              </div>
+              <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+                <div className="flex gap-x-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-11 text-primary"
+                  >
+                    <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
+                  </svg>
+                  <dd className="text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">100k+</dd>
+                </div>
+                <dt className="text-base leading-7 text-gray-600">Served costumers</dt>
+              </div>
+              <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+                <div className="flex gap-x-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-11 text-primary"
+                  >
+                    <path d="M4.5 3.75a3 3 0 0 0-3 3v.75h21v-.75a3 3 0 0 0-3-3h-15Z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M22.5 9.75h-21v7.5a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-7.5Zm-18 3.75a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 0 1.5h-6a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <dd className="text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">46k+</dd>
+                </div>
+                <dt className="text-base leading-7 text-gray-600">Members</dt>
+              </div>
+              <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+                <div className="flex gap-x-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-11 text-primary"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <dd className="text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">24</dd>
+                </div>
+                <dt className="text-base leading-7 text-gray-600">Open Hours</dt>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
